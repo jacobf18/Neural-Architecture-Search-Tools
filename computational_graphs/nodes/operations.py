94460435eebf731e4ff743ded26bc404c400830a
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2022-06-03 19:02:53 -0400

    made relu and flatten operations

--- computational_graphs/nodes/operations.py
+++ computational_graphs/nodes/operations.py
@@ -1,4 +1,5 @@
 from .node import Node
+import numpy as np
 
 class BinaryOperation(Node):
     def __init__(self, parent1, parent2):
@@ -46,3 +47,22 @@ class MultiplyElementWise(BinaryOperation):
     
     def __str__(self) -> str:
         return f'Multiply {self.parents[0]} by scalar {self.parents[1]}'
+    
+
+class ReLU(UnaryOperation):
+    def get_output_shape(self):
+        return self.parents[0].get_output_shape()
+    
+    def check_if_parents_valid(self):
+        return True
+    
+    def __str__(self) -> str:
+        return f'ReLU on input of shape {self.get_output_shape()}'
+    
+class Flatten(UnaryOperation):
+    def get_output_shape(self):
+        parent_shape = self.parents[0].get_output_shape()
+        if len(parent_shape) >= 2:
+            return (parent_shape[0], np.prod(parent_shape[1:]))
+        else:
+            return parent_shape
